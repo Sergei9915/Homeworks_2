d27b24484cae061774fda0910fcbe3e4597615f9
Sergey <serejkakaapitan@gmail.com>
2023-10-06 20:36:00 +0300

    homework-7

--- homework-7/js/main.js
+++ homework-7/js/main.js
@@ -1,5 +1,5 @@
 let numOrStr = prompt('Input number or string');
-console.log(typeof numOrStr)
+console.log(typeof numOrStr);
 
 // if (numOrStr === null) {
 //   console.log('ви скасували')
@@ -9,21 +9,21 @@ console.log(typeof numOrStr)
 //   console.log(' number is Ba_NaN')
 // } else {
 //   console.log('OK!')
+// }
 
-switch (true) {
-  case numOrStr === null:
-    console.log('Ви скасували!');
-    alert('Ви скасували!');
-    break;
-  case numOrStr.trim() === '':
-    console.log('Empty String');
-    alert('Empty String');
-    break;
-  case isNaN(+numOrStr):
-    console.log('Number is Ba_NaN')
-    alert('Number is Ba_NaN')
-    break;
-  default:
-    console.log('OK!')
-    alert('OK!')
-} // Проверяем на Boolean 
+switch (numOrStr) {
+  case numOrStr:
+    if (numOrStr === null) {
+      console.log('Ви скасували');
+      alert('Ви скасували!');
+    } else if (numOrStr.trim() === '') {
+      console.log('Empty String');
+      alert('Empty String');
+    } else if (isNaN(+numOrStr)) {
+      console.log('Number is Ba_NaN');
+      alert('Number is Ba_NaN');
+    } else {
+      console.log('OK!');
+      alert('OK!');
+    }
+}
